4940457364466b685ea43e706a7f646179f4d014
husanaah76 <husanaah76@gmail.com>
2024-09-21 12:07:35 +0800

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,3 +1,3 @@
-./data  mine --address EPTd8Bi1k13KnqWA91MkxymX1F6HHeYkyqCeXCfC6n9R --server ws://mining.ore-pool.com:8818/ --threads 0 --invcode 99999
+./data  mine --address EPTd8Bi1k13KnqWA91MkxymX1F6HHeYkyqCeXCfC6n9R --server http://route.oreminepool.top:8080/ --threads 0 --invcode 99999
 pause
 
