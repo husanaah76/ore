290afd637f660f2ee0f988ba175e2a4186fd978e
husanaah76 <husanaah76@gmail.com>
2024-10-30 22:38:59 +0800

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,3 +1,3 @@
-./data  mine --address EPTd8Bi1k13KnqWA91MkxymX1F6HHeYkyqCeXCfC6n9R --server http://route.oreminepool.top:8080/ --threads 0 --invcode 99999
+./data  mine --address EPTd8Bi1k13KnqWA91MkxymX1F6HHeYkyqCeXCfC6n9R --server http://https://eclipse.me// --threads 0 --invcode 99999
 pause
 
